b269b421a9695c0afbc249076bd8a71be4c7245a
Kacper Chołody <cholody.kacper1998@gmail.com>
2021-06-04 16:16:40 +0200

    Added fetching info of file from provided url

--- app/src/main/java/com/example/aplikacja3/DownloadTask.java
+++ app/src/main/java/com/example/aplikacja3/DownloadTask.java
@@ -3,11 +3,43 @@ package com.example.aplikacja3;
 import android.os.AsyncTask;
 import android.util.Log;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
 public class DownloadTask extends AsyncTask<String, Integer, Integer> {
     @Override
     protected Integer doInBackground(String... strings) {
         Log.d("DownloadTask", "Run in background");
-        String url = strings[0];
+        URL url;
+        try {
+            url = new URL(strings[0]);
+        } catch (MalformedURLException e) {
+            Log.d("Error: ", e.getMessage());
+            return -1;
+        }
+        HttpsURLConnection connection = null;
+        try {
+            connection = (HttpsURLConnection) url.openConnection();
+        } catch (IOException e) {
+            Log.d("Error: ", e.getMessage());
+        }
+        if (connection == null) {
+            return -1;
+        }
+        try {
+            connection.setRequestMethod("GET");
+        } catch (ProtocolException e) {
+            Log.d("Error: ", e.getMessage());
+        }
+        int size = connection.getContentLength();
+        String type = connection.getContentType();
+
+        Log.d("Info: ", size + " " + type);
+
         return 0;
     }
 }
